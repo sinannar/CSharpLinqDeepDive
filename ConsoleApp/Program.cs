a3e4775965a138eb9fa25041429c747432f0eb0b
Sinan Nar <sinan.nar@gmail.com>
2024-05-07 19:47:54 +1200

    benchmark dotnet used

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,121 +1,163 @@
-﻿using System.Collections;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections;
 using System.Diagnostics;
-using System.Reflection.Metadata.Ecma335;
 
-IEnumerable<int> source = Enumerable.Range(0, 1000).ToArray();
 
 //for (int i = 0; i < 10_000; ++i)
 //{
 //    foreach (int value in SelectManual(source, x => x * 2)) { }
 //}
 
-Console.WriteLine(Enumerable.Select(source, x => x * 2).Sum());
+//Console.WriteLine(Enumerable.Select(source, x => x * 2).Sum());
 
-var c = SelectCompiler(source, x => x * 2);
-Console.WriteLine(c.Sum());
-Console.WriteLine(c.Sum());
+//var c = SelectCompiler(source, x => x * 2);
+//Console.WriteLine(c.Sum());
+//Console.WriteLine(c.Sum());
 
-var m = SelectManual(source, x => x * 2);
-Console.WriteLine(m.Sum());
-Console.WriteLine(m.Sum());
+//var m = SelectManual(source, x => x * 2);
+//Console.WriteLine(m.Sum());
+//Console.WriteLine(m.Sum());
 
-static IEnumerable<TResult> SelectCompiler<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+[MemoryDiagnoser]
+[ShortRunJob]
+public class Tests
 {
-    ArgumentNullException.ThrowIfNull(source);
-    ArgumentNullException.ThrowIfNull(selector);
+    private IEnumerable<int> source = Enumerable.Range(0, 1000).ToArray();
 
-    return Impl(source, selector);
-
-    static IEnumerable<TResult> Impl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+    [Benchmark]
+    public int SumCompiler()
     {
+        int sum = 0;
+        foreach (int i in SelectCompiler(source, i => i * 2)) {
+            sum += i;
+        }
+        return sum;
+    }
 
-        foreach (var item in source)
+
+    [Benchmark]
+    public int SumManual()
+    {
+        int sum = 0;
+        foreach (int i in SelectManual(source, i => i * 2))
         {
-            yield return selector(item);
+            sum += i;
+        }
+        return sum;
+    }
+
+    [Benchmark]
+    public int SumLinq()
+    {
+        int sum = 0;
+        foreach (int i in Enumerable.Select(source, i => i * 2))
+        {
+            sum += i;
+        }
+        return sum;
+    }
+
+    static IEnumerable<TResult> SelectCompiler<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        return Impl(source, selector);
+
+        static IEnumerable<TResult> Impl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+
+            foreach (var item in source)
+            {
+                yield return selector(item);
+            }
         }
     }
-}
 
-static IEnumerable<TResult> SelectManual<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
-{
-    ArgumentNullException.ThrowIfNull(source);
-    ArgumentNullException.ThrowIfNull(selector);
-
-    return new SelectManualEnumerable<TSource, TResult>(source, selector);
-}
-
-class SelectManualEnumerable<TSource, TResult> : IEnumerable<TResult>, IEnumerator<TResult>
-{
-    private IEnumerable<TSource> _source;
-    private Func<TSource, TResult> _selector;
-
-    private int _threadId = Environment.CurrentManagedThreadId;
-    private TResult _current = default!;
-    private IEnumerator<TSource>? _enumerator;
-    private int _state = 0;
-
-    public SelectManualEnumerable(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+    static IEnumerable<TResult> SelectManual<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
     {
-        _source = source;
-        _selector = selector;
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        return new SelectManualEnumerable<TSource, TResult>(source, selector);
     }
 
-    public IEnumerator<TResult> GetEnumerator()
+    class SelectManualEnumerable<TSource, TResult> : IEnumerable<TResult>, IEnumerator<TResult>
     {
-        //if(Interlocked.CompareExchange(ref _state, 1, 0) == 0)
-        if(_threadId == Environment.CurrentManagedThreadId && _state == 0)
+        private IEnumerable<TSource> _source;
+        private Func<TSource, TResult> _selector;
+
+        private int _threadId = Environment.CurrentManagedThreadId;
+        private TResult _current = default!;
+        private IEnumerator<TSource>? _enumerator;
+        private int _state = 0;
+
+        public SelectManualEnumerable(IEnumerable<TSource> source, Func<TSource, TResult> selector)
         {
-            _state = 1;
-            return this;
+            _source = source;
+            _selector = selector;
         }
-        return new SelectManualEnumerable<TSource, TResult>(_source, _selector) { _state = 1};
-    }
 
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-    public TResult Current => _current;
-
-    object? IEnumerator.Current => Current;
-
-    public bool MoveNext()
-    {
-        switch(_state)
+        public IEnumerator<TResult> GetEnumerator()
         {
-            case 1:
-                _enumerator = _source.GetEnumerator();
-                _state = 2;
-                goto case 2;
-            case 2:
-                Debug.Assert(_enumerator is not null);
-                try
-                {
-                    if (_enumerator.MoveNext())
+            //if(Interlocked.CompareExchange(ref _state, 1, 0) == 0)
+            if (_threadId == Environment.CurrentManagedThreadId && _state == 0)
+            {
+                _state = 1;
+                return this;
+            }
+            return new SelectManualEnumerable<TSource, TResult>(_source, _selector) { _state = 1 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public TResult Current => _current;
+
+        object? IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            switch (_state)
+            {
+                case 1:
+                    _enumerator = _source.GetEnumerator();
+                    _state = 2;
+                    goto case 2;
+                case 2:
+                    Debug.Assert(_enumerator is not null);
+                    try
                     {
-                        _current = _selector(_enumerator.Current);
-                        return true;
-                        // yield return
+                        if (_enumerator.MoveNext())
+                        {
+                            _current = _selector(_enumerator.Current);
+                            return true;
+                            // yield return
+                        }
                     }
-                }
-                catch
-                {
-                    Dispose();
-                    throw;
-                }
-                break;
+                    catch
+                    {
+                        Dispose();
+                        throw;
+                    }
+                    break;
+            }
+
+            Dispose();
+            return false;
         }
 
-        Dispose();
-        return false;
-    }
+        public void Dispose()
+        {
+            _state = -1;
+            _enumerator?.Dispose();
+        }
 
-    public void Dispose()
-    {
-        _state = -1;
-        _enumerator?.Dispose();
-    }
-
-    public void Reset()
-    {
-        throw new NotSupportedException();
+        public void Reset()
+        {
+            throw new NotSupportedException();
+        }
     }
 }
