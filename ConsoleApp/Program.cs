49ab372bf9f30583b3be8938caa52b93cdf5b8e4
Sinan Nar <sinan.nar@gmail.com>
2024-05-07 16:19:52 +1200

    when to run

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,20 @@
 ﻿
-IEnumerable<int> source = new List<int>() { 1, 2, 3 };
-foreach (var i in Select(source, i => i * 2))
-{
-    Console.WriteLine(i);
-}
+//Enumerable.Select<int, int>(null, i => i * 2);
+
+
+//Console.WriteLine(0);
+IEnumerable<int> e = Select<int, int>(null, i => i * 2);
+//Console.WriteLine(1);
+//IEnumerator<int> enumarator = e.GetEnumerator();
+//Console.WriteLine(2);
+//enumarator.MoveNext();
+//Console.WriteLine(3);
 
 static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
 {
+    ArgumentNullException.ThrowIfNull(source);
+    ArgumentNullException.ThrowIfNull(selector);
+
     foreach (var item in source)
     {
         yield return selector(item);
