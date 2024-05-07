85666cd59eef0a825cc0bb5806b2877d6c3f58f9
Sinan Nar <sinan.nar@gmail.com>
2024-05-07 16:20:08 +1200

    when to run

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,13 +2,13 @@
 //Enumerable.Select<int, int>(null, i => i * 2);
 
 
-//Console.WriteLine(0);
+Console.WriteLine(0);
 IEnumerable<int> e = Select<int, int>(null, i => i * 2);
-//Console.WriteLine(1);
-//IEnumerator<int> enumarator = e.GetEnumerator();
-//Console.WriteLine(2);
-//enumarator.MoveNext();
-//Console.WriteLine(3);
+Console.WriteLine(1);
+IEnumerator<int> enumarator = e.GetEnumerator();
+Console.WriteLine(2);
+enumarator.MoveNext();
+Console.WriteLine(3);
 
 static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
 {
