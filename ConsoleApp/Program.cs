927c80b38ebf8c3d51cc59949c027994cb0fe7e2
Sinan Nar <sinan.nar@gmail.com>
2024-05-07 15:29:58 +1200

    Select

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,6 +14,11 @@
 //}
 
 
+IEnumerable<int> source = new List<int>() { 1, 2, 3 };
+foreach (var i in Select(source, i => i * 2))
+{
+    Console.WriteLine(i);
+}
 
 static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
 {
